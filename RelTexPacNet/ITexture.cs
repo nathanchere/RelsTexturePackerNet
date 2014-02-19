7935a333ed19e19582438b6728bbaf48da2299d9
Nathan Chere <git@nathanchere.com.au>
2014-02-20 08:53:21 +1100

    Support for tests

---
+++ RelTexPacNet/ITexture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace RelTexPacNet
+{
+    public interface ITexture
+    {
+        byte[] Bitmap { get; }
+        int Width { get; }
+        int Height { get; set; }
+
+    }
+}
