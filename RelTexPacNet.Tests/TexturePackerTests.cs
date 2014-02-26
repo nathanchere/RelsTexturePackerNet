43886a7e37fb75c5e92aa03ae2a6ba1d2c220d27
Nathan Chere <git@nathanchere.com.au>
2014-02-26 19:39:12 +1100

    Failing tests

--- RelTexPacNet.Tests/TexturePackerTests.cs
+++ RelTexPacNet.Tests/TexturePackerTests.cs
@@ -7,14 +7,9 @@ namespace RelTexPacNet
 {
     public class TexturePackerTests
     {
-        private ITexture MockImage(int height, int width)
+        private Image MockImage(int height, int width)
         {
-            var result = new Mock<ITexture>();
-            result.SetupGet(x => x.Height).Returns(height);
-            result.SetupGet(x => x.Width).Returns(width);
-            var blankImage = new Bitmap(width, height);
-            result.SetupGet(x => x.Image).Returns(blankImage);
-            return result.Object;
+            return new Bitmap(width, height);
         }
 
         // No no no no no
@@ -22,31 +17,73 @@ namespace RelTexPacNet
         [Fact]
         public void Run_with_no_input_returns_WasSuccessful_false()
         {
-            Assert.True(false);
+            var settings = new TexturePacker.Settings
+            {
+            };
+
+            var packer = new TexturePacker(settings);
+            var result = packer.Run();
+
+            Assert.False(result.WasSuccessful);
         }
 
         [Fact]
         public void Run_with_bad_input_returns_WasSuccessful_false()
         {
-            Assert.True(false);
+            var settings = new TexturePacker.Settings
+            {
+            };
+
+            var packer = new TexturePacker(settings);
+            packer.AddImage(null, "");
+            var result = packer.Run();
+
+            Assert.False(result.WasSuccessful);
         }
 
         [Fact]
         public void Run_with_no_input_returns_appropriate_error_message()
         {
-            Assert.True(false);
+            var settings = new TexturePacker.Settings
+            {
+            };
+
+            var packer = new TexturePacker(settings);
+            var result = packer.Run();
+
+            Assert.Equal(result.ErrorMessage, "No input images provided");
         }
 
-        [Fact]
+        [Fact] //TODO: define what is bad input - reference, image
         public void Run_with_bad_input_returns_appropriate_error_message()
         {
-            Assert.True(false);
+            var settings = new TexturePacker.Settings
+            {
+            };
+
+            var packer = new TexturePacker(settings);
+            packer.AddImage(null, "a");
+
+            var result = packer.Run();
+
+            Assert.Equal(result.ErrorMessage, "Cannot process null values");
         }
 
         [Fact]
         public void Run_with_valid_input_returns_no_error_message()
         {
-            Assert.True(false);
+            var settings = new TexturePacker.Settings
+            {
+            };
+
+            var packer = new TexturePacker(settings);
+            packer.AddImage(new Bitmap(120, 60), "a");
+            packer.AddImage(new Bitmap(80, 100), "b");
+            packer.AddImage(new Bitmap(32, 32), "c");
+
+            var result = packer.Run();
+
+            Assert.Equal(result.ErrorMessage, "");
         }
 
         [Fact]
@@ -57,9 +94,9 @@ namespace RelTexPacNet
             };
 
             var packer = new TexturePacker(settings);
-            packer.AddImage(MockImage(120, 60), "a");
-            packer.AddImage(MockImage(64, 128), "b");
-            packer.AddImage(MockImage(32, 32), "c");
+            packer.AddImage(new Bitmap(120, 60), "a");
+            packer.AddImage(new Bitmap(80, 100), "b");
+            packer.AddImage(new Bitmap(32, 32), "c");
 
             var result = packer.Run();
 
@@ -74,16 +111,37 @@ namespace RelTexPacNet
             };
 
             var packer = new TexturePacker(settings);
-            
-            const int inputImageCount = 3;
-
-            packer.AddImage(MockImage(120, 60), "a");
-            packer.AddImage(MockImage(64, 128), "b");
-            packer.AddImage(MockImage(32, 32), "c");
+            packer.AddImage(new Bitmap(120, 60), "a");
+            packer.AddImage(new Bitmap(80, 100), "b");
+            packer.AddImage(new Bitmap(32, 32), "c");
 
             var result = packer.Run();
 
-            Assert.Equal(result.TextureAtlas.Nodes.Count(), inputImageCount);
+            Assert.True(result.Nodes.Count == 3);
+            Assert.True(result.Nodes.Count(n => n.Reference == "a") == 1);
+            Assert.True(result.Nodes.Count(n => n.Reference == "b") == 1);
+            Assert.True(result.Nodes.Count(n => n.Reference == "c") == 1);
+        }
+
+        [Fact]
+        public void Run_success_result_only_contains_explicitly_added_node_references()
+        {
+            var settings = new TexturePacker.Settings
+            {
+            };
+
+            var packer = new TexturePacker(settings);
+            packer.AddImage(new Bitmap(120, 60), "a");
+            packer.AddImage(new Bitmap(80, 100), "b");
+            packer.AddImage(new Bitmap(32, 32), "c");
+
+            var result = packer.Run();
+
+            var wrongCount = result.Nodes
+                .Select(n => n.Reference)
+                .Count(s => !new[] {"a", "b", "c"}.Contains(s));
+
+            Assert.Equal(wrongCount, 0);
         }
     }
 }
