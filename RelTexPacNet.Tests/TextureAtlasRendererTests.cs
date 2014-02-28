b78e90f294549d9cdc7936d4cf39fab2a80bc540
Nathan Chere <git@nathanchere.com.au>
2014-02-28 15:04:03 +1100

    TextureAtlasRenderer implementation

---
+++ RelTexPacNet.Tests/TextureAtlasRendererTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using Moq;
+using Xunit;
+
+namespace RelTexPacNet
+{
+    public class TextureAtlasRendererTests
+    {
+        [Fact]
+        private void This_is_a_shit_excuse_for_a_test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RelTexPacNet/TextureAtlasRenderer.cs
+++ RelTexPacNet/TextureAtlasRenderer.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace RelTexPacNet
@@ -20,8 +22,27 @@ namespace RelTexPacNet
     {
         public Bitmap Render(TextureAtlas atlas)
         {
-            
+            var result = new Bitmap(
+                atlas.Size.Width,
+                atlas.Size.Height,
+                PixelFormat.Format32bppPArgb
+            );            
 
+            using (var g = Graphics.FromImage(result))
+            {
+                g.Clear(atlas.MatteColor);
+
+                atlas.Nodes.ToList().ForEach(n => {
+                    // Rotate image if required
+                    // if(condition) n.Texture.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+                    g.DrawImage(n.Texture, n.X, n.Y);
+                });
+            }
+
+            return result;
         }
+
+        
     }
 }
