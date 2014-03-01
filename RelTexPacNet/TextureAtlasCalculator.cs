6921d28cfcce3e102e29abf9ff863b9694ec6dec
Nathan Chere <git@nathanchere.com.au>
2014-03-01 21:06:18 +1100

    Atlas calculator shuffle

--- RelTexPacNet/TextureAtlasCalculator.cs
+++ RelTexPacNet/TextureAtlasCalculator.cs
@@ -14,6 +14,9 @@ namespace RelTexPacNet
         {
             public Size Size { get; set; }
             public int Padding { get; set; }
+            public bool IsRotationEnabled { get; set; }
+
+            // TODO: other options like pre-sort by longest side
         }
 
         private Settings _settings;
@@ -27,36 +30,39 @@ namespace RelTexPacNet
 
         public void Add(Image image, string reference)
         {
-            if (image == null) throw new ArgumentNullException("Cannot add null images");
-            if (string.IsNullOrWhiteSpace(reference)) throw new ArgumentNullException("reference cannot be empty");
-            
-            int maxLength = Math.Max(
-                _settings.Size.Width - _settings.Padding * 2,
-                _settings.Size.Height - _settings.Padding * 2
-                );
-
-            if(image.Width > maxLength)
-                throw new ArgumentOutOfRangeException("Image width excees atlas working area");
-            if (image.Height > maxLength)
-                throw new ArgumentOutOfRangeException("Image height excees atlas working area");
+            ValidateInput(image, reference);
 
             _nodes.Add(reference, new TextureAtlasNode
             {
                 Texture = image,
                 Reference = reference,
             } );
+        }
 
-            // TODO: calculate X Y
+        private void ValidateInput(Image image, string reference)
+        {
+            if (image == null) throw new ArgumentNullException("Cannot add null images");
+            if (string.IsNullOrWhiteSpace(reference)) throw new ArgumentNullException("reference cannot be empty");
+
+            int maxLength = Math.Max(
+                _settings.Size.Width - _settings.Padding * 2,
+                _settings.Size.Height - _settings.Padding * 2
+                );
+
+            if (image.Width > maxLength)
+                throw new ArgumentOutOfRangeException("Image width excees atlas working area");
+            if (image.Height > maxLength)
+                throw new ArgumentOutOfRangeException("Image height excees atlas working area");
         }
 
         public TextureAtlas Calculate()
         {
             if (!_nodes.Any()) throw new InvalidOperationException("No input textures provided");
-
-            var texture = new Bitmap(_settings.Size.Width, _settings.Size.Height);
+            
             var nodes = _nodes.Values.ToList();
             
-            // work out where they all go
+            
+
 
             return new TextureAtlas
             {
