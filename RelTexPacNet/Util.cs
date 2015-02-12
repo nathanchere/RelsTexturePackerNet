7f0255604271807ef80f8f1519fb360b7bf3b572
Nathan Chere <git@nathanchere.com.au>
2015-02-12 22:55:20 +0100

    Fix brainfart bug

--- RelTexPacNet/Util.cs
+++ RelTexPacNet/Util.cs
@@ -171,7 +171,7 @@ namespace RelTexPacNet
 
         public static bool IsEntirelyContainedBy(this Rectangle rect, Rectangle target)
         {
-            return rect.Left < target.Left || rect.Right > target.Right || rect.Top < target.Top || rect.Bottom > target.Bottom;
+            return !(rect.Left < target.Left || rect.Right > target.Right || rect.Top < target.Top || rect.Bottom > target.Bottom);
         }
 
         // Assumes no rectangles are overlapping beyond their edges
